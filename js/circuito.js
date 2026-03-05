ab403240f26e2cffd4e386cc4f1cd1aafb7f6f02
Colasnic <yatevargas@gmail.com>
2026-03-05 00:01:46 -0500

    Revision_tabla
    falta crear 1 tabla del codigo

--- js/circuito.js
+++ js/circuito.js
@@ -96,13 +96,24 @@ window.onclick = function(event) {
    ==================================== */
 document.addEventListener("DOMContentLoaded", function () {
     const modal = document.getElementById("codeModal");
+    const arduinoModal = document.getElementById("arduinoModal"); // Modal del código Arduino
     const imageModal = document.getElementById("imageModal");
-    if (modal ) {
+    const otroModal = document.getElementById("otroModal"); // ← ejemplo de otro modal
+
+    if (modal) {
         modal.style.display = "none";
+    }
+
+    if (arduinoModal) {
+        arduinoModal.style.display = "none";
     }
 
     if (imageModal) {
         imageModal.style.display = "none";
+    }
+
+    if (otroModal) {    // ← nuevo if agregado
+        otroModal.style.display = "none";
     }
 });
 
@@ -178,3 +189,4 @@ function closeImageModal() {
     var modal = document.getElementById('imageModal');
     modal.style.display = 'none';
 }
+
